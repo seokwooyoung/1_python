7e20a6c8718e0fe523a73706a04916a7c48e683b
seokwooyoung <dndud05066@gmail.com>
2022-11-08 15:01:16 +0900

    and_or commit

---
+++ and_or.py
@@ -0,0 +1,23 @@
+s = 'banana'
+
+"""
+if 'a' in s:
+    if 'b' in s:
+        print ('banana에는 a도 있고, b도 있다.')
+    else:
+        print ('banana에는 a는 있지만, b는 없다.')
+else:
+    print('banana에는 a, b 둘다 없다.')
+"""
+
+"""
+# and
+if 'a' in s and 'b' in s:
+    print('banana에는 a도 있고, b도 있어요')
+"""
+
+"""
+# or
+if 'a' in s or 'c' in s:
+    print('banana에는 a와 c 둘중 하나는 있어요.')
+"""
---
+++ while2.py
@@ -0,0 +1,8 @@
+# 제곱 수 계산
+num = int(input('정수를 입력하시오. : '))
+i = 1
+
+while i <= num:
+    j = i * i
+    print(i,j)
+    i = i + 1

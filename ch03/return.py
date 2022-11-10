4acdace8a43ecb787d89c0e65285e6e45de6209f
seokwooyoung <dndud05066@gmail.com>
2022-11-10 10:21:05 +0900

    return commit

---
+++ ch03/return.py
@@ -0,0 +1,14 @@
+# 한국어로 표기, return 사용
+
+def korea_number(i):
+    if i == 1:
+        return '일'
+    elif i == 2:
+        return '이'
+    elif i == 3:
+        return '삼'
+    elif i == 4:
+        return '사'
+
+korea_number(3)
+        

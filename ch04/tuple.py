5b2af45388243dd90070976490de12fcc555d954
seokwooyoung <dndud05066@gmail.com>
2022-11-11 09:20:03 +0900

    commit

---
+++ ch04/tuple.py
@@ -0,0 +1,24 @@
+"""
+a = 10
+b = 20
+temp = a    #temp = 10
+a = b       #a = 20
+b = temp    #b = 10
+
+print(a, b)
+"""
+"""
+def magu_print(x, y, *rest):    # '*'을 붙이면 그 이후에 들어오는 것을 하나의 튜플로
+    print(x, y, rest)
+
+magu_print(1,2,3,4,5,6,7,8)
+"""
+
+#튜플 p -> 리스트 l
+p = (1,2,3)
+l = list(p)
+print(l)
+
+#리스트 l -> 튜플 p
+p = tuple(l)
+print(p)

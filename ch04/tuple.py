07993d2bccdd3df2066e06270d63550b6e340a54
seokwooyoung <dndud05066@gmail.com>
2022-11-14 17:20:27 +0900

    tuple commit

--- ch04/tuple.py
+++ ch04/tuple.py
@@ -8,7 +8,7 @@ b = temp    #b = 10
 print(a, b)
 """
 """
-def magu_print(x, y, *rest):    # '*'을 붙이면 그 이후에 들어오는 것을 하나의 튜플로
+def magu_print(x, y, *rest):    # '*'을 붙이면 그 이후에 들어오는 것을 하나의 튜플로 처리
     print(x, y, rest)
 
 magu_print(1,2,3,4,5,6,7,8)
@@ -17,8 +17,9 @@ magu_print(1,2,3,4,5,6,7,8)
 #튜플 p -> 리스트 l
 p = (1,2,3)
 l = list(p)
-print(l)
+print(l)    #[1,2,3]
 
 #리스트 l -> 튜플 p
 p = tuple(l)
-print(p)
+print(p)    #(1,2,3)
+
